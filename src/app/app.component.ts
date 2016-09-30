51b33149110f35c140bf7923fb29db2143f94159
Rey Hernandez <reyvhernandez@gmail.com>
2016-09-30 17:32:59 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import {Component} from "@angular/core";
+import {Observable} from "rxjs";
+import {Store} from "@ngrx/store";
+import {INCREMENT, DECREMENT, RESET} from "./counter";
+
+interface AppState {
+  counter: number;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+        <button (click)="increment()">Increment</button>
+        <div>Current Count: {{ counter | async }}</div>
+        <button (click)="decrement()">Decrement</button>
+    `
+})
+export class MyApp {
+  counter: Observable<number>;
+
+  constructor(public store: Store<AppState>) {
+    this.counter = store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch({type: INCREMENT});
+  }
+
+  decrement() {
+    this.store.dispatch({type: DECREMENT});
+  }
+
+  reset() {
+    this.store.dispatch({type: RESET});
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { Store, StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter';
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    StoreModule.provideStore({ counter: counterReducer }, { counter: 0 })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: []
+})
+export class AppModule {}
